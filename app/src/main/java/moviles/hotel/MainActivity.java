9a3c5cbff985ca81820d853d574f8d03c48890e1
Dayana Pulido <144375079+Bygyacqq@users.noreply.github.com>
2024-05-20 17:16:13 -0500

    parcial

--- app/src/main/java/moviles/hotel/MainActivity.java
+++ app/src/main/java/moviles/hotel/MainActivity.java
@@ -3,6 +3,7 @@ package moviles.hotel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,14 +13,14 @@ public class MainActivity extends AppCompatActivity {
     private HotelDBHelper dbHelper;
     private Button btnReserveSpa, btnCalculateBill;
 
-    private EditText etUsername, etPassword;
 
-    private Button btnLogin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
 
     }
 }
